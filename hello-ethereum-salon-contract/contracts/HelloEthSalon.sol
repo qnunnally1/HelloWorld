487438ce38d8159404f5c9790466bdc55df127e3
Q.Nunnally <qnunnally1@student.gsu.edu>
2019-03-08 20:10:14 -0500

    Smart contract setup.

---
+++ hello-ethereum-salon-contract/contracts/HelloEthSalon.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+
+contract HelloEthSalon {
+  string message = "Hello Ethereum Salon!";
+
+  constructor() public {
+  }
+
+  function GetMessage() returns (string) {
+    return message;
+  }
+}
